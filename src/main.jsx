e2a5ccead67f20e45fc12277669b5119c4dd5593
Vadim <vadig2018@mail.ru>
2025-05-22 15:06:20 +0300

    Integrate Prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/App/App.jsx";
 import "./styles/index.css";
-import { ThemeProvider } from "./components/App/ThemeContext.jsx";
+import { ThemeProvider } from "./components/ThemeContext.jsx";
+
 createRoot(document.getElementById("root")).render(
   <>
     <ThemeProvider>
